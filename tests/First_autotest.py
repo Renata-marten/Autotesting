d1c18543c8cfdb4a45843037b94a325e543668b0
Renata-marten <ren_ish@mail.ru>
2020-09-19 20:28:48 +0500

    16-09-4

--- tests/First_autotest.py
+++ tests/First_autotest.py
@@ -12,3 +12,12 @@ class TestCalc:
 
    def test_multiply_failed(self):
        assert self.calc.multiply(self, 2, 2) == 5
+
+   def test_division_calculate_correctly(self):
+       assert self.calc.division(self, 8, 2) == 4
+
+   def test_subtraction_calculate_correctly(self):
+       assert self.calc.subtraction(self, 5, 2) == 3
+
+   def test_adding_calculate_correctly(self):
+       assert self.calc.adding(self, 3, 2) == 5
